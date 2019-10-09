0006feee6b857818bc111f75b0c7237c2fbd6eed
(s) Thomas Allenbrook <thomas.allenbrook@students.plymouth.ac.uk>
2019-10-09 11:57:26 +0100

    Tests set up

---
+++ Week02/MyYear/test/classification/StageResultsTest.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classification;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author tallenbrook
+ */
+public class StageResultsTest {
+    
+    public StageResultsTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testGetStage2Average() {
+    }
+
+    @Test
+    public void testGetTotalCredits() {
+    }
+
+    @Test
+    public void testGetTotalMarks() {
+    }
+
+    @Test
+    public void testSetStage2Average() {
+    }
+
+    @Test
+    public void testIsComplete() {
+        fail("Test not yet implemented");
+    }
+
+    @Test
+    public void testAddModuleMark() {
+    }
+
+    @Test
+    public void testCalculateAverageSoFar() {
+    }
+
+    @Test
+    public void testPredictClass() {
+    }
+
+    @Test
+    public void testResetValues() {
+    }
+    
+}
